f72e0589066887e1b55bf4255dedf7bd2ae658b4
andharsm <andharsirajmunir@gmail.com>
2023-01-10 10:55:41 +0700

    fix Data barang

--- InventoriApp-Karanglewas/FormDataBarang.cs
+++ InventoriApp-Karanglewas/FormDataBarang.cs
@@ -23,7 +23,7 @@ namespace InventoriApp_Karanglewas
         {
             InitializeComponent();
         }
-        private void show()
+        /*private void show()
         {
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = conn;
@@ -36,6 +36,56 @@ namespace InventoriApp_Karanglewas
             dgvDB.DataSource = ds;
             dgvDB.DataMember = "data_barang";
             dgvDB.ReadOnly = true;
+        }*/
+
+        private DataTable getDataDB()
+        {
+            try
+            {
+                dataTable.Reset();
+                dataTable = new DataTable();
+                conn.Open();
+                string query = "SELECT " +
+                            "tb_kategori.jenis_kategori AS Kategori, " +
+                            "tb_barang.nama_barang AS Barang, " +
+                            "CASE WHEN masuk.total_masuk IS NULL THEN 0 ELSE masuk.total_masuk END AS Masuk, " +
+                            "CASE WHEN keluar.total_keluar IS NULL THEN 0 ELSE keluar.total_keluar END AS Keluar, " +
+                            "CASE WHEN (masuk.total_masuk - keluar.total_keluar) IS NULL THEN 0 ELSE (masuk.total_masuk - keluar.total_keluar) END AS Total " +
+                        "FROM tb_barang " +
+                        "LEFT JOIN tb_kategori ON tb_kategori.id_kategori = tb_barang.id_kategori " +
+                        "LEFT JOIN ( " +
+                            "SELECT " +
+                                "tb_barangmasuk.id_kategori, " +
+                                "tb_barangmasuk.id_barang, " +
+                                "SUM(tb_barangmasuk.jumlah) as total_masuk " +
+                            "FROM tb_barangmasuk " +
+                            "GROUP BY tb_barangmasuk.id_kategori,tb_barangmasuk.id_barang " +
+                        ") masuk ON masuk.id_kategori = tb_kategori.id_kategori AND masuk.id_barang = tb_barang.id_barang " +
+                        "LEFT JOIN ( " +
+                            "SELECT " +
+                                "tb_barangkeluar.id_kategori, " +
+                                "tb_barangkeluar.id_barang, " +
+                                "SUM(tb_barangkeluar.jumlah) as total_keluar " +
+                            "FROM tb_barangkeluar " +
+                            "GROUP BY tb_barangkeluar.id_kategori,tb_barangkeluar.id_barang " +
+                        ") keluar ON keluar.id_kategori = tb_kategori.id_kategori AND keluar.id_barang = tb_barang.id_barang";
+                cmd = new SqlCommand(query, conn);
+                reader = cmd.ExecuteReader();
+                dataTable.Load(reader);
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dataTable;
+        }
+
+        private void fillDataBarang()
+        {
+
+            dgvDB.DataSource = getDataDB();
         }
 
         private void cbKategoridb()
@@ -53,8 +103,8 @@ namespace InventoriApp_Karanglewas
 
         private void FormDataBarang_Load(object sender, EventArgs e)
         {
-            show();
-            //fillDataBarang();
+            //show();
+            fillDataBarang();
         }
 
         private void btTambah_Click(object sender, EventArgs e)
@@ -91,8 +141,8 @@ namespace InventoriApp_Karanglewas
 
         private void btnrefres_Click(object sender, EventArgs e)
         {
-            show();
-            //fillDataBarang();
+            //show();
+            fillDataBarang();
         }
     }
 }
