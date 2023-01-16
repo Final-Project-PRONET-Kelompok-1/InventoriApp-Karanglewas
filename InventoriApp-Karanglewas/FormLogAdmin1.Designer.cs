a6f025d467caf5993342d68dded9cc70b6c4bea3
zain1005 <zainarif1005@gmail.com>
2023-01-17 00:29:08 +0700

    add form logadmin1

--- InventoriApp-Karanglewas/FormLogAdmin1.Designer.cs
+++ InventoriApp-Karanglewas/FormLogAdmin1.Designer.cs
@@ -28,20 +28,231 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle19 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle20 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelFilterText = new System.Windows.Forms.Panel();
+            this.btCariTextRwt = new System.Windows.Forms.Button();
+            this.txtFilterRiwayat = new System.Windows.Forms.TextBox();
+            this.panelFilterTanggal = new System.Windows.Forms.Panel();
+            this.btCariTanggal = new System.Windows.Forms.Button();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.cbFilterRiwayat = new System.Windows.Forms.ComboBox();
+            this.dataRiwayat = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panelFilterText.SuspendLayout();
+            this.panelFilterTanggal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataRiwayat)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.LightBlue;
+            this.panel1.Controls.Add(this.panelFilterText);
+            this.panel1.Controls.Add(this.panelFilterTanggal);
+            this.panel1.Controls.Add(this.cbFilterRiwayat);
+            this.panel1.Location = new System.Drawing.Point(2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(595, 206);
+            this.panel1.TabIndex = 4;
+            // 
+            // panelFilterText
+            // 
+            this.panelFilterText.Controls.Add(this.btCariTextRwt);
+            this.panelFilterText.Controls.Add(this.txtFilterRiwayat);
+            this.panelFilterText.Location = new System.Drawing.Point(143, 9);
+            this.panelFilterText.Margin = new System.Windows.Forms.Padding(2);
+            this.panelFilterText.Name = "panelFilterText";
+            this.panelFilterText.Size = new System.Drawing.Size(182, 32);
+            this.panelFilterText.TabIndex = 21;
+            // 
+            // btCariTextRwt
+            // 
+            this.btCariTextRwt.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btCariTextRwt.Font = new System.Drawing.Font("Lucida Sans", 9F);
+            this.btCariTextRwt.Location = new System.Drawing.Point(133, 0);
+            this.btCariTextRwt.Margin = new System.Windows.Forms.Padding(2);
+            this.btCariTextRwt.Name = "btCariTextRwt";
+            this.btCariTextRwt.Size = new System.Drawing.Size(49, 20);
+            this.btCariTextRwt.TabIndex = 21;
+            this.btCariTextRwt.Text = "Cari";
+            this.btCariTextRwt.UseVisualStyleBackColor = true;
+            this.btCariTextRwt.Click += new System.EventHandler(this.btCariTextRwt_Click);
+            // 
+            // txtFilterRiwayat
+            // 
+            this.txtFilterRiwayat.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtFilterRiwayat.Font = new System.Drawing.Font("Lucida Sans", 9F);
+            this.txtFilterRiwayat.Location = new System.Drawing.Point(0, 0);
+            this.txtFilterRiwayat.Margin = new System.Windows.Forms.Padding(2);
+            this.txtFilterRiwayat.Name = "txtFilterRiwayat";
+            this.txtFilterRiwayat.Size = new System.Drawing.Size(133, 22);
+            this.txtFilterRiwayat.TabIndex = 22;
+            this.txtFilterRiwayat.TextChanged += new System.EventHandler(this.txtFilterRiwayat_TextChanged);
+            // 
+            // panelFilterTanggal
+            // 
+            this.panelFilterTanggal.Controls.Add(this.btCariTanggal);
+            this.panelFilterTanggal.Controls.Add(this.dateTimePicker3);
+            this.panelFilterTanggal.Controls.Add(this.label6);
+            this.panelFilterTanggal.Controls.Add(this.dateTimePicker2);
+            this.panelFilterTanggal.Location = new System.Drawing.Point(147, 10);
+            this.panelFilterTanggal.Margin = new System.Windows.Forms.Padding(2);
+            this.panelFilterTanggal.Name = "panelFilterTanggal";
+            this.panelFilterTanggal.Size = new System.Drawing.Size(437, 24);
+            this.panelFilterTanggal.TabIndex = 23;
+            // 
+            // btCariTanggal
+            // 
+            this.btCariTanggal.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btCariTanggal.Font = new System.Drawing.Font("Lucida Sans", 9F);
+            this.btCariTanggal.Location = new System.Drawing.Point(379, 0);
+            this.btCariTanggal.Margin = new System.Windows.Forms.Padding(2);
+            this.btCariTanggal.Name = "btCariTanggal";
+            this.btCariTanggal.Size = new System.Drawing.Size(58, 20);
+            this.btCariTanggal.TabIndex = 19;
+            this.btCariTanggal.Text = "Cari";
+            this.btCariTanggal.UseVisualStyleBackColor = true;
+            this.btCariTanggal.Click += new System.EventHandler(this.btCariTanggal_Click);
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dateTimePicker3.Font = new System.Drawing.Font("Lucida Sans", 9F);
+            this.dateTimePicker3.Location = new System.Drawing.Point(196, 0);
+            this.dateTimePicker3.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(183, 22);
+            this.dateTimePicker3.TabIndex = 20;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label6.Location = new System.Drawing.Point(183, 0);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "--";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dateTimePicker2.Font = new System.Drawing.Font("Lucida Sans", 9F);
+            this.dateTimePicker2.Location = new System.Drawing.Point(0, 0);
+            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(183, 22);
+            this.dateTimePicker2.TabIndex = 19;
+            // 
+            // cbFilterRiwayat
+            // 
+            this.cbFilterRiwayat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.cbFilterRiwayat.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFilterRiwayat.FormattingEnabled = true;
+            this.cbFilterRiwayat.Items.AddRange(new object[] {
+            "Semua Data",
+            "By Tanggal",
+            "By Text"});
+            this.cbFilterRiwayat.Location = new System.Drawing.Point(10, 10);
+            this.cbFilterRiwayat.Margin = new System.Windows.Forms.Padding(2);
+            this.cbFilterRiwayat.Name = "cbFilterRiwayat";
+            this.cbFilterRiwayat.Size = new System.Drawing.Size(121, 23);
+            this.cbFilterRiwayat.TabIndex = 18;
+            this.cbFilterRiwayat.SelectedIndexChanged += new System.EventHandler(this.cbFilterRiwayat_SelectedIndexChanged);
+            // 
+            // dataRiwayat
+            // 
+            this.dataRiwayat.AllowUserToOrderColumns = true;
+            this.dataRiwayat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataRiwayat.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataRiwayat.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataRiwayat.BackgroundColor = System.Drawing.Color.White;
+            this.dataRiwayat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataRiwayat.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle17.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(129)))), ((int)(((byte)(194)))));
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataRiwayat.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle17;
+            this.dataRiwayat.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle18.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle18.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(129)))), ((int)(((byte)(194)))));
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.Color.LightBlue;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(129)))), ((int)(((byte)(194)))));
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataRiwayat.DefaultCellStyle = dataGridViewCellStyle18;
+            this.dataRiwayat.GridColor = System.Drawing.Color.White;
+            this.dataRiwayat.Location = new System.Drawing.Point(14, 46);
+            this.dataRiwayat.Margin = new System.Windows.Forms.Padding(2);
+            this.dataRiwayat.Name = "dataRiwayat";
+            this.dataRiwayat.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            dataGridViewCellStyle19.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle19.BackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle19.Font = new System.Drawing.Font("Lucida Sans", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle19.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle19.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle19.SelectionForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle19.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataRiwayat.RowHeadersDefaultCellStyle = dataGridViewCellStyle19;
+            this.dataRiwayat.RowHeadersWidth = 51;
+            dataGridViewCellStyle20.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle20.ForeColor = System.Drawing.Color.Black;
+            this.dataRiwayat.RowsDefaultCellStyle = dataGridViewCellStyle20;
+            this.dataRiwayat.RowTemplate.Height = 24;
+            this.dataRiwayat.Size = new System.Drawing.Size(572, 544);
+            this.dataRiwayat.TabIndex = 5;
             // 
             // FormLogAdmin1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.dataRiwayat);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormLogAdmin1";
             this.Text = "FormLogAdmin1";
+            this.panel1.ResumeLayout(false);
+            this.panelFilterText.ResumeLayout(false);
+            this.panelFilterText.PerformLayout();
+            this.panelFilterTanggal.ResumeLayout(false);
+            this.panelFilterTanggal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataRiwayat)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panelFilterText;
+        private System.Windows.Forms.Button btCariTextRwt;
+        private System.Windows.Forms.TextBox txtFilterRiwayat;
+        private System.Windows.Forms.Panel panelFilterTanggal;
+        private System.Windows.Forms.Button btCariTanggal;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ComboBox cbFilterRiwayat;
+        private System.Windows.Forms.DataGridView dataRiwayat;
     }
 }
--- InventoriApp-Karanglewas/FormLogAdmin1.cs
+++ InventoriApp-Karanglewas/FormLogAdmin1.cs
@@ -2,19 +2,166 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace InventoriApp_Karanglewas
 {
+    
     public partial class FormLogAdmin1 : Form
     {
+        SqlConnection conn = dbConfig.conn;
+        SqlCommand cmd;
+        SqlDataReader reader;
+        string date1;
+        string date2;
+        DateTime date;
+        DataTable dataTable = new DataTable();
         public FormLogAdmin1()
         {
             InitializeComponent();
+            fillDataRiwayat();
+
+            cbFilterRiwayat.Text = "Semua Data";
         }
+        private DataTable tampilSemua()
+        {
+            try
+            {
+                dataTable.Reset();
+                dataTable = new DataTable();
+                conn.Open();
+                string query = "SELECT a.username AS Username, l.keterangan AS Keterangan, l.waktu\n" +
+                    "FROM tb_log l\n" +
+                    "LEFT JOIN tb_admin a ON a.id_admin = l.id_admin\n" +
+                    "ORDER BY waktu DESC";
+                reader = cmd.ExecuteReader();
+                dataTable.Load(reader);
+                conn.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return dataTable;
+        }
+
+        private void fillDataRiwayat()
+        {
+            dataRiwayat.DataSource = tampilSemua();
+        }
+        private void resetDTP()
+        {
+            date = DateTime.Now;
+            dateTimePicker2.Text = date.ToString();
+            dateTimePicker3.Text = date.ToString();
+        }
+
+        private void cbFilterRiwayat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbFilterRiwayat.Text == "Semua Data")
+            {
+                fillDataRiwayat();
+                panelFilterTanggal.Visible = false;
+                panelFilterText.Visible = false;
+
+                resetDTP();
+                txtFilterRiwayat.Text = "";
+            }
+            else if (cbFilterRiwayat.Text == "By Tanggal")
+            {
+                panelFilterTanggal.Visible = true;
+                panelFilterText.Visible = false;
+                resetDTP();
+                txtFilterRiwayat.Text = "";
+            }
+
+            else if (cbFilterRiwayat.Text == "By Text")
+            {
+                panelFilterTanggal.Visible = false;
+                panelFilterText.Visible = true;
+
+                resetDTP();
+                fillDataRiwayat();
+            }
+        }
+            private void filterByText()
+            {
+                try
+                {
+
+                    dataTable.Reset();
+                    dataTable = new DataTable();
+                    conn.Open();
+                string query = "SELECT a.username AS Username, l.keterangan AS Keterangan, l.waktu AS Tanggal\n" +
+                    "FROM tb_log l\n" +
+                    "LEFT JOIN tb_admin a ON a.id_admin = l.id_admin\n" +
+                    "WHERE a.username LIKE '%" + txtFilterRiwayat.Text + "%' OR l.keterangan LIKE '%" + txtFilterRiwayat.Text + "%' OR l.waktu LIKE '%" + txtFilterRiwayat.Text + "%'\n" +
+                    "ORDER BY waktu DESC";
+                                    
+                    cmd = new SqlCommand(query, conn);
+                    reader = cmd.ExecuteReader();
+                    dataTable.Load(reader);
+                    conn.Close();
+
+                    dataRiwayat.DataSource = dataTable;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        private void txtFilterRiwayat_TextChanged(object sender, EventArgs e)
+        {
+            if (txtFilterRiwayat.Text == "")
+            {
+                fillDataRiwayat();
+            
+            }
+        }
+
+        private void btCariTextRwt_Click(object sender, EventArgs e)
+        {
+            filterByText();
+        }
+
+        private void btCariTanggal_Click(object sender, EventArgs e)
+        {
+            date1 = dateTimePicker2.Value.Year + "-" + dateTimePicker2.Value.Month + "-" + dateTimePicker2.Value.Day;
+            date2 = dateTimePicker3.Value.Year + "-" + dateTimePicker3.Value.Month + "-" + dateTimePicker3.Value.Day;
+            try
+            {
+
+                dataTable.Reset();
+                dataTable = new DataTable();
+                conn.Open();
+                string query = "SELECT a.username AS Username, l.keterangan AS Keterangan, l.waktu AS Tanggal\n" +
+                    "FROM tb_log l\n" +
+                    "LEFT JOIN tb_admin a ON a.id_admin = l.id_admin\n" +
+                    "WHERE CAST(l.waktu as DATE) BETWEEN '" + date1 + "' AND '" + date2 + "' \n" +
+                    "ORDER BY waktu DESC";
+                cmd = new SqlCommand(query, conn);
+                reader = cmd.ExecuteReader();
+                dataTable.Load(reader);
+                conn.Close();
+
+                dataRiwayat.DataSource = dataTable;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    
     }
+    
 }
